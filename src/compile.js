8942e48842a786165eb09393a4c72f1e3481a83a
Shifeng Huang <shifeng.huang@hpe.com>
2016-05-24 09:51:58 +0000

    $directive ...

--- src/compile.js
+++ src/compile.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var _ = require('lodash');
+var $ = require('jquery');
+
+function nodeName(node) {
+  return node.nodeName ? node.nodeName : node[0].nodeName;
+}
 
 function $CompileProvider($provide) {
 
@@ -26,10 +31,45 @@ function $CompileProvider($provide) {
     }
   };
 
-  this.$get = function () {
+  this.$get = ['$injector', function ($injector) {
 
-  };
+    function compile($compileNodes) {
+      return compileNodes($compileNodes);
+    }
+
+    function compileNodes($compileNodes) {
+      _.forEach($compileNodes, function(node) {
+        var directives = collectDirectives(node);
+        applyDirectivesToNode(directives, node);
+      });
+    }
+
+    function applyDirectivesToNode(directives, compileNode) {
+      var $compileNode = $(compileNode);
+      _.forEach(directives, function (directive) {
+        if (directive.compile) {
+          directive.compile($compileNode);
+        }
+      })
+    }
+
+    function collectDirectives(node) {
+      var directives = [];
+
+      var normalizedNodeName = _.camelCase(nodeName(node).toLowerCase());
+      addDirective(directives, normalizedNodeName);
+      return directives;
+    }
+
+    function addDirective(directives, name) {
+      if (hasDirectives.hasOwnProperty(name)) {
+        directives.push.apply(directives, $injector.get(name + 'Directive'));
+      }
+    }
+
+    return compile;
+  }];
 }
-$CompileProvider.$inject = ['$provide']
+$CompileProvider.$inject = ['$provide'];
 
 module.exports = $CompileProvider;
